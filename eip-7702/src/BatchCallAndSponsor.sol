86ca0e04527355ae0e13219bf13ec7f33d873404
royalnine <17271567+royalnine@users.noreply.github.com>
2025-04-10 17:52:11 +0500

    before oz

--- eip-7702/src/BatchCallAndSponsor.sol
+++ eip-7702/src/BatchCallAndSponsor.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+// import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 /**
  * @title BatchCallAndSponsor
